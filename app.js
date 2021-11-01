9d7bf97b5d1543db1d38c2dc63d4c0af7ded4869
[Elia <[elitoribio01@gmail.com]>
2021-11-01 17:36:47 -0400

    Media queries on Tablet

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-
+var wrapperMenu = document.querySelector('.wrapper-menu');
+const gsap_animation = document.querySelector(".gsap_animation");
+const overlay = document.querySelector(".overlay");
+const hamburger_menu = document.querySelector(".hamburger_menu");
 
 // document.getElementById("label_menu").addEventListener("click", NoScroll);
 //         function NoScroll() {
@@ -21,36 +24,6 @@
 //             window.onscroll = function() {};
 //        } 
 //     } 
-
-
-// const menu = document.querySelector(".menu");
-// const menuItems = document.querySelectorAll(".menuItem");
-// const hamburger= document.querySelector(".hamburger");
-// const closeIcon= document.querySelector(".closeIcon");
-// const menuIcon = document.querySelector(".menuIcon");
-
-// const gsap_animation = document.querySelector(".gsap_animation");
-// const overlay = document.querySelector(".overlay");
-// const hamburger_menu = document.querySelector(".hamburger_menu");
-// const ul_menu = document.querySelector(".ul_menu");
-//  function toggleMenu() {
-//   if (gsap_animation.classList.contains("showMenu")) {
-//     gsap_animation.classList.remove("showMenu");
-//     closeIcon.style.display = "none";
-//     wrapperMenu.style.display = "block";
-
-    // Animation menu with gsap
-    // gsap_animation.style.visibility = "visible";
-    // overlay.style.visibility = "visible";
-    // hamburger_menu.style.visibility = "visible";
-
-    //   gsap.set(".gsap_animation", {left: '100%'});
-    //   gsap.set(".overlay", {left: '100%'});
-    //   gsap.set(".hamburger_menu", {left:'100%', opacity:0});
-
-    //   gsap.to(".gsap_animation", {left: '0%', duration:1, ease: "expo.out", delay: 0.4});
-    //   gsap.to(".overlay", {left: '0%', duration:1, ease: "expo.out",  delay: 0.5});
-    //   gsap.to(".hamburger_menu", {left:'0%', opacity: 1, duration:1, ease: "expo.out", delay: 0.7});
 
 
       
@@ -82,30 +55,27 @@
 
 //   }
 // }
-var wrapperMenu = document.querySelector('.wrapper-menu');
-const gsap_animation = document.querySelector(".gsap_animation");
-const overlay = document.querySelector(".overlay");
-const hamburger_menu = document.querySelector(".hamburger_menu");
+
 // const ul_menu = document.querySelector(".ul_menu");
 
-wrapperMenu.addEventListener('click', function(){
-wrapperMenu.classList.toggle('open');  
-    gsap_animation.style.visibility = "visible";
-    overlay.style.visibility = "visible";
-    hamburger_menu.style.visibility = "visible";
+// wrapperMenu.addEventListener('click', function(){
+// wrapperMenu.classList.toggle('open');  
+//     gsap_animation.style.visibility = "visible";
+//     overlay.style.visibility = "visible";
+//     hamburger_menu.style.visibility = "visible";
 
 
-      gsap.set(".gsap_animation", {left: '100%'});
-      gsap.set(".overlay", {left: '100%'});
-      gsap.set(".hamburger_menu", {left:'100%', opacity:0});
+//       gsap.set(".gsap_animation", {left: '100%'});
+//       gsap.set(".overlay", {left: '100%'});
+//       gsap.set(".hamburger_menu", {left:'100%', opacity:0});
 
-      gsap.to(".gsap_animation", {left: '0%', duration:1, ease: "expo.out", delay: 0.07});
-      gsap.to(".overlay", {left: '0%', duration:1, ease: "expo.out",  delay: 0.08});
-      gsap.to(".hamburger_menu", {left:'0%', opacity: 1, duration:1, ease: "expo.out", delay: 0.09});
+//       gsap.to(".gsap_animation", {left: '0%', duration:1, ease: "expo.out", delay: 0.07});
+//       gsap.to(".overlay", {left: '0%', duration:1, ease: "expo.out",  delay: 0.08});
+//       gsap.to(".hamburger_menu", {left:'0%', opacity: 1, duration:1, ease: "expo.out", delay: 0.09});
   
  
   
-})
+// })
 
 
 // Gsap Animations
@@ -122,20 +92,6 @@ wrapperMenu.classList.toggle('open');
 // });
 // End of ScrollTrigger
 
-  
-// Preba
-
-// gsap.set(".gsap_animation", {left: '100%'});
-// gsap.set(".overlay", {left: '100%'});
-// gsap.set(".hamburger_menu", {left:'100%', opacity:0});
-
-// gsap.to(".gsap_animation", {left: '0%', duration:1, ease: "expo.out", delay: 1.2});
-
-// gsap.to(".overlay", {left: '0%', duration:1, ease: "expo.out",  delay: 1.4});
-// gsap.to(".hamburger_menu", {left:'0%', opacity: 1, duration:1, ease: "bounce.out", delay: 2});
-
-
-// end of preba
 
   gsap.from('.menuIcon', 1.5,{
     opacity: 0,
@@ -170,11 +126,139 @@ wrapperMenu.classList.toggle('open');
 
   gsap.from('#wrapper', 1.5,{
         opacity: 0,
-        // x: 40,
-        // x: '-100%',
         delay: 1,
         stagger: 1,
         ease: Power4.easeOut
       });
+
+//About Animation 
+  gsap.fromTo(".svg_silla", {
+    opacity: 0}, 
+    {opacity: 1, 
+    duration: 0.9, 
+    delay: 0.3,
+    ease: Expo.easeInOut
+    });
+
+    gsap.fromTo(".svg_boy", {
+      opacity: 0}, 
+      {opacity: 1, 
+        duration: 1,
+       delay: 0.5,
+      ease: Expo.easeInOut
+      });
+
+
+ 
+  //work
+  wrapperMenu.addEventListener('click', function(){
+  if (wrapperMenu.classList.toggle('open')) {
+    gsap_animation.style.visibility = "visible";
+    overlay.style.visibility = "visible";
+    hamburger_menu.style.visibility = "visible";
+
+    gsap.set(".gsap_animation", {left: '100%'});
+    gsap.set(".overlay", {left: '100%'});
+    gsap.set(".hamburger_menu", {left:'100%', opacity:0});
+
+    gsap.to(".gsap_animation", {left: '0%', duration:1, ease: "expo.out", delay: 0.07});
+    gsap.to(".overlay", {left: '0%', duration:1, ease: "expo.out",  delay: 0.08});
+    gsap.to(".hamburger_menu", {left:'0%', opacity: 1, duration:1, ease: "expo.out", delay: 0.09});
+
+    alert("open");
+  }else{
+    alert("close");
+
+    gsap.set(".gsap_animation", {left: '100%'});
+    gsap.set(".overlay",{left: '100%'});
+    gsap.set(".hamburger_menu", {left:'100%'});
   
+    gsap.from(".gsap_animation", {left: '0%', duration: 0.8, ease: "expo.out", opacity: 0.9});
+    gsap.from(".overlay", {left: '0%', duration: 0.9, ease: "expo.out"});
+    gsap.from(".hamburger_menu", {left: '0%', duration: 1, ease: "expo.out",});
+
+
+  // setTimeout(function(){ 
+  //   alert("Ya"); 
+  //   gsap_animation.style.visibility = "hidden";
+  //   overlay.style.visibility = "hidden";
+  //   hamburger_menu.style.visibility = "hidden";
+  // }, 1000);
+
+//     gsap_animation.style.visibility = "hidden";
+//     overlay.style.visibility = "hidden";
+//     hamburger_menu.style.visibility = "hidden";
+  }
+});
+ //Form validation
+
+ const Name = document.getElementById('name');
+ const email = document.getElementById('email');
+ const details = document.getElementById('details');
+ const form = document.getElementById('form');
+ const paragraph = document.getElementById('warning');
+
+
+ form.addEventListener("submit", e=>{
+   e.preventDefault()
+   let warnings = ""
+   let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+   let textEnter = false;
+
+    if (Name.value.length < 4) {
+      warnings += `Invalid name. Too short<br>`
+      Name.focus()
+      console.log(regexEmail.test(email.value));
+     textEnter = true; 
+    } 
+    if (!regexEmail.test(email.value)) {
+     warnings += `Invalid email.<br>`
+     textEnter = true
+    }
+
+    if (textEnter) {
+     paragraph.innerHTML = warnings
+    }
+ });
+//end of form
+
+//
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+});
+function valida_envia(){
+  //valido el nombre
+  if (document.fvalida.nombre.value.length==0){
+       alert("Tiene que escribir su nombre")
+       document.fvalida.nombre.focus()
+       return 0;
+  }
+
+  //valido la edad. tiene que ser entero mayor que 18
+  edad = document.fvalida.edad.value
+  edad = validarEntero(edad)
+  document.fvalida.edad.value=edad
+  if (edad==""){
+       alert("Tiene que introducir un número entero en su edad.")
+       document.fvalida.edad.focus()
+       return 0;
+  }else{
+       if (edad<18){
+            alert("Debe ser mayor de 18 años.")
+            document.fvalida.edad.focus()
+            return 0;
+       }
+  }
+
+  //valido el interés
+  if (document.fvalida.interes.selectedIndex==0){
+       alert("Debe seleccionar un motivo de su contacto.")
+       document.fvalida.interes.focus()
+       return 0;
+  }
+
+  //el formulario se envia
+  alert("Muchas gracias por enviar el formulario");
+  document.fvalida.submit();
+}
 
